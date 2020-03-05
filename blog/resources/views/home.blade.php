e8cb46918fa24e2f57db79d193ccd805212634e8
Caio Reis Guimarães <caio.guimaraes@prodepa.pa.gov.br>
2020-03-05 09:19:32 -0300

    criando o projeto laravel

---
+++ blog/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Example Component</div>
+
+                <div class="panel-body">
+                    VIDA LONGA AO REI!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
